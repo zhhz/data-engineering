6bfc3ffc88b6988dfb2df6a53fc472814294c836
Zhonghai Zuo <zhonghai.zuo@gmail.com>
2014-05-09 20:16:02 -0400

    fix: toggle the upload btn based on if a file was chosen

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  $("#merchant_file_file").change(function(){
+      if($(this).val() !== ''){
+        $("#submit-btn").removeAttr('disabled');
+      }
+  });
+});
